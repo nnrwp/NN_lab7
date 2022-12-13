37a8d3e5a7ba6527e1d7e08c194e63b84ab6c661
nnrwp <rawipa_s@cmu.ac.th>
2022-12-13 16:26:22 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,29 @@
 
 using namespace std;
 
+int adiff(int A, int B){
+  int C;
+  if(A > B){
+    C = A-B;
+  }else{
+    C = B-A;
+  }
+  if(C%360 > 180){
+    return 360 - C%360;
+  }else{
+    return C%360;
+  }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;
+
+  return 0;
 }
