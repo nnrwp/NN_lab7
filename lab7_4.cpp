81eab2dc0bac6e8857086eec60f55d6fec0ef088
nnrwp <rawipa_s@cmu.ac.th>
2022-12-13 16:19:03 +0700

    lab7_4.cpp completed

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,15 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
-		
+    int i = 0;
+	double g;
+	cout << "Please input GPA: ";
+	cin >> g;
+	while(i < 10){
+		if(gpa[i] >= g){
+			cout << id[i] << ' ' << name[i] << ' '<< gpa[i] <<"\n";
+		}
+		i = i+1;
+	}
 	return 0;
 }
